263e118d09353ee8bf263b83509ffa8dc0918254
Irakli Adamashvili <adamashvili.irakli8@gmail.com>
2025-03-21 17:55:57 +0400

    add footer and error page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from './navbar/navbar.component';
 import { RouterModule } from '@angular/router';
+import { FooterComponent } from "./footer/footer.component";
 
 
 
 
 @Component({
   selector: 'app-root',
-  imports: [NavbarComponent, RouterModule ],
+  imports: [NavbarComponent, RouterModule, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,13 @@ import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { GalleryComponent } from './gallery/gallery.component';
 import { ContactsComponent } from './contacts/contacts.component';
+import { ErrorpageComponent } from './errorpage/errorpage.component';
 
 
 export const routes: Routes = [
     {path: "", component: HomeComponent},
-    {path: "about", component: AboutComponent},
+    {path: "about", component: AboutComponent},    
     {path: "gallery", component: GalleryComponent},
-    {path: "contacts", component: ContactsComponent}
+    {path: "contacts", component: ContactsComponent},
+    {path: "**", component: ErrorpageComponent},
 ];
